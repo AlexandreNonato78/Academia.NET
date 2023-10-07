92319c81bbe0bd80b01d0655d53d19a070a25dd2
Alexandre Nonato <101572814+AlexandreNonato78@users.noreply.github.com>
2023-10-07 09:58:46 -0300

    Add files via upload

---
+++ 051023ExercicioRepetição7/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _051023ExercicioRepetição7;
+//7. Escreva um programa em C# leia 20 números e que conte a quantidade de números pares e ímpares digitados por 
+//um usuário.Apresente o resultado.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int numerosPares = 0;
+        int numerosImpares = 0;
+
+        for (int i = 1; i <= 20; i++)
+        {
+            Console.Write("Digite o número " + i + ": ");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero % 2 == 0)
+            {
+                numerosPares++;
+            }
+            else
+            {
+                numerosImpares++;
+            }
+        }
+
+        Console.WriteLine("Quantidade de números pares: " + numerosPares);
+        Console.WriteLine("Quantidade de números ímpares: " + numerosImpares);
+    }
+}
