e48795a57881a546e3bd337129c194b3be2c9745
Alexandre Nonato <101572814+AlexandreNonato78@users.noreply.github.com>
2023-10-07 09:56:13 -0300

    Add files via upload

---
+++ 051023ExercicioRepetição5/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _051023ExercicioRepetição5;
+//5. Escreva um algoritmo para calcular o fatorial de um número.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um número para calcular o fatorial: ");
+        int n = int.Parse(Console.ReadLine());
+
+        long fatorial = 1;
+
+        for (int i = 1; i <= n; i++)
+        {
+            fatorial *= i;
+        }
+
+        Console.WriteLine("O fatorial de " + n + " é: " + fatorial);
+    }
+}
