5453ed48a727e8eef53ff82ecfe83553f9720c87
Alexandre Nonato <101572814+AlexandreNonato78@users.noreply.github.com>
2023-10-09 15:39:23 -0300

    Add files via upload

---
+++ 061023_exercicioRepeticao_pt2_13/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _061023_exercicioRepeticao_pt2_13;
+//13. Elaborar um programa que apresente o somatório dos valores pares existentes
+//entre 1 e 500.
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int somatorio = 0;
+
+        for (int i = 2; i <= 500; i += 2)
+        {
+            somatorio += i;
+        }
+
+        Console.WriteLine($"O somatório dos valores pares entre 1 e 500 é: {somatorio}");
+    }
+}
