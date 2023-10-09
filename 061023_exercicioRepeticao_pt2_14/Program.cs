f52f3d9858b94d5c3d25be0dca72af8821a10af4
Alexandre Nonato <101572814+AlexandreNonato78@users.noreply.github.com>
2023-10-09 15:40:21 -0300

    Add files via upload

---
+++ 061023_exercicioRepeticao_pt2_14/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _061023_exercicioRepeticao_pt2_14;
+    //14. Mostrar as potências de 2 variando de 0 a 10.
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int i = 0; i <= 10; i++)
+        {
+            double resultado = Math.Pow(2, i);
+            Console.WriteLine($"2^{i} = {resultado}");
+        }
+    }
+}
