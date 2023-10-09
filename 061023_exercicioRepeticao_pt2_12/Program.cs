9f972f575ba8860f9eb77dd8961775ae329396a8
Alexandre Nonato <101572814+AlexandreNonato78@users.noreply.github.com>
2023-10-09 15:38:37 -0300

    Add files via upload

---
+++ 061023_exercicioRepeticao_pt2_12/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.ConstrainedExecution;
+
+namespace _061023_exercicioRepeticao_pt2_12;
+//12. Apresentar o total da soma dos cem primeiros números inteiros.
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = 100;
+        int soma = (n * (n + 1)) / 2;
+        Console.WriteLine($"A soma dos {n} primeiros números inteiros é: {soma}");
+    }
+}
