eead091291379023db402d9d01887f78c76b41ed
Alexandre Nonato <101572814+AlexandreNonato78@users.noreply.github.com>
2023-10-10 15:33:53 -0300

    Add files via upload

---
+++ 061023_exercicioRepeticao_pt2_18/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _061023_exercicioRepeticao_pt2_18;
+
+//18. Elaborar um programa que apresente o valor de uma potência de uma base qualquer
+//elevada a um expoente qualquer, ou seja, NM.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite a base (N): ");
+        double baseN = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite o expoente (M): ");
+        int expoenteM = int.Parse(Console.ReadLine());
+
+        double resultado = Math.Pow(baseN, expoenteM);
+
+        Console.WriteLine($"O valor de {baseN}^{expoenteM} é igual a {resultado}");
+    }
+}
