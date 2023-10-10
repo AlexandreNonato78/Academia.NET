aa0f509512aed4aa54e0fbeed799f4f357272d28
Alexandre Nonato <101572814+AlexandreNonato78@users.noreply.github.com>
2023-10-10 15:33:09 -0300

    Add files via upload

---
+++ 061023_exercicioRepeticao_pt2_17/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _061023_exercicioRepeticao_pt2_17;
+//17. Apresentar os quadrados dos números inteiros de 15 a 200.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Quadrados dos números inteiros de 15 a 200:");
+
+        for (int i = 15; i <= 200; i++)
+        {
+            int quadrado = i * i;
+            Console.WriteLine($"O quadrado de {i} é {quadrado}");
+        }
+    }
+}
