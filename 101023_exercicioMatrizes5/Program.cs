2f2667f30a0a22eec40f2961edf03b6ec79f41b5
Alexandre Nonato <101572814+AlexandreNonato78@users.noreply.github.com>
2023-10-16 08:42:05 -0300

    Add files via upload

---
+++ 101023_exercicioMatrizes5/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace _101023_exercicioMatrizes5;
+
+//5) Leia duas matrizes 2x3 de números double. 
+//Imprima a soma destas duas matrizes.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int linhas = 2;
+        int colunas = 3;
+
+        double[,] matriz1 = new double[linhas, colunas];
+        double[,] matriz2 = new double[linhas, colunas];
+        double[,] matrizSoma = new double[linhas, colunas];
+
+        // Preencha a primeira matriz
+        Console.WriteLine("Preencha a primeira matriz:");
+        for (int i = 0; i < linhas; i++)
+        {
+            for (int j = 0; j < colunas; j++)
+            {
+                Console.Write($"Digite o valor para a posição [{i},{j}]: ");
+                matriz1[i, j] = Convert.ToDouble(Console.ReadLine());
+            }
+        }
+
+        // Preencha a segunda matriz
+        Console.WriteLine("Preencha a segunda matriz:");
+        for (int i = 0; i < linhas; i++)
+        {
+            for (int j = 0; j < colunas; j++)
+            {
+                Console.Write($"Digite o valor para a posição [{i},{j}]: ");
+                matriz2[i, j] = Convert.ToDouble(Console.ReadLine());
+            }
+        }
+
+        // Calcule a matriz de soma
+        for (int i = 0; i < linhas; i++)
+        {
+            for (int j = 0; j < colunas; j++)
+            {
+                matrizSoma[i, j] = matriz1[i, j] + matriz2[i, j];
+            }
+        }
+
+        // Imprima a matriz de soma
+        Console.WriteLine("\nMatriz de Soma:");
+        for (int i = 0; i < linhas; i++)
+        {
+            for (int j = 0; j < colunas; j++)
+            {
+                Console.Write(matrizSoma[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
