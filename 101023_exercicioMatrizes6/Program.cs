cc9bc12096a5188e7b0d21b883b523551d5e68c9
Alexandre Nonato <101572814+AlexandreNonato78@users.noreply.github.com>
2023-10-16 09:00:08 -0300

    Add files via upload

---
+++ 101023_exercicioMatrizes6/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _101023_exercicioMatrizes6;
+
+//6) Gere e imprima uma matriz M 4x4 com valores aleatórios entre 0-9. Após isso determine o maior número da matriz.
+//Random random = new Random();
+//int randomNumber = random.Next(0, 100);
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[,] matriz = new int[4, 4];
+        Random random = new Random();
+        int maiorNumero = int.MinValue;
+
+        Console.WriteLine("Matriz 4x4 com valores aleatórios entre 0 e 9:");
+
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                matriz[i, j] = random.Next(0, 10);
+                Console.Write(matriz[i, j] + " ");
+
+                // Verifica e atualiza o maior número
+                if (matriz[i, j] > maiorNumero)
+                {
+                    maiorNumero = matriz[i, j];
+                }
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("\nO maior número na matriz é: " + maiorNumero);
+    }
+}
