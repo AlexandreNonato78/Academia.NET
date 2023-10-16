2733b2aa2eaa2ac6908bda9c00497ac58046b8a6
Alexandre Nonato <101572814+AlexandreNonato78@users.noreply.github.com>
2023-10-16 09:08:31 -0300

    Add files via upload

---
+++ 101023_exercicioMatrizes12/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _101023_exercicioMatrizes12;
+
+//12) Escreva um programa que leia os valores de uma matriz 4x3, e em seguida
+//mostre na tela apenas os valores cuja soma dos índices(i+j) seja um número par.
+
+using System;
+class Program
+{
+    static void Main()
+    {
+        int linhas = 4;
+        int colunas = 3;
+
+        int[,] matriz = new int[linhas, colunas];
+
+        Console.WriteLine("Preencha a matriz 4x3:");
+
+        // Preencha a matriz
+        for (int i = 0; i < linhas; i++)
+        {
+            for (int j = 0; j < colunas; j++)
+            {
+                Console.Write($"Digite o valor para a posição [{i},{j}]: ");
+                matriz[i, j] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        Console.WriteLine("Valores cuja soma dos índices (i+j) é par:");
+
+        // Mostre os valores com soma de índices par
+        for (int i = 0; i < linhas; i++)
+        {
+            for (int j = 0; j < colunas; j++)
+            {
+                if ((i + j) % 2 == 0)
+                {
+                    Console.WriteLine($"Valor na posição [{i},{j}]: {matriz[i, j]}");
+                }
+            }
+        }
+    }
+}
