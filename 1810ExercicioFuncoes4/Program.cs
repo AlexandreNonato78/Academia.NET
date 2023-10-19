9d1cbcce16772e1775bc0ffd509715e2f5448114
Alexandre Nonato <101572814+AlexandreNonato78@users.noreply.github.com>
2023-10-19 10:31:40 -0300

    Add files via upload

---
+++ 1810ExercicioFuncoes4/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _1810ExercicioFuncoes4
+//4) Escreva uma função que calcule o fatorial de um número inteiro positivo.
+
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Digite um número inteiro positivo: ");
+            if (int.TryParse(Console.ReadLine(), out int numero) && numero >= 0)
+            {
+                long fatorial = CalcularFatorial(numero);
+                Console.WriteLine($"O fatorial de {numero} é {fatorial}");
+            }
+            else
+            {
+                Console.WriteLine("Entrada inválida. Por favor, insira um número inteiro positivo.");
+            }
+        }
+
+        static long CalcularFatorial(int numero)
+        {
+            if (numero == 0 || numero == 1)
+            {
+                return 1;
+            }
+            long fatorial = 1;
+            for (int i = 2; i <= numero; i++)
+            {
+                fatorial *= i;
+            }
+            return fatorial;
+        }
+    }
+
+}
