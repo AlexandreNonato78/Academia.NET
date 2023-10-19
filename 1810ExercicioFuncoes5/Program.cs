d9eb7f09aaeb740b7df5b483cdb7552372824189
Alexandre Nonato <101572814+AlexandreNonato78@users.noreply.github.com>
2023-10-19 10:32:23 -0300

    Add files via upload

---
+++ 1810ExercicioFuncoes5/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _1810ExercicioFuncoes5
+    //5) Desenvolva uma função que converta uma temperatura em graus Celsius para Fahrenheit.
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Digite a temperatura em graus Celsius: ");
+            if (double.TryParse(Console.ReadLine(), out double celsius))
+            {
+                double fahrenheit = ConverterCelsiusParaFahrenheit(celsius);
+                Console.WriteLine($"{celsius} graus Celsius equivalem a {fahrenheit} graus Fahrenheit.");
+            }
+            else
+            {
+                Console.WriteLine("Entrada inválida. Por favor, insira um valor numérico válido.");
+            }
+        }
+
+        static double ConverterCelsiusParaFahrenheit(double celsius)
+        {
+            // Fórmula de conversão de Celsius para Fahrenheit
+            return (celsius * 9 / 5) + 32;
+        }
+    }
+}
