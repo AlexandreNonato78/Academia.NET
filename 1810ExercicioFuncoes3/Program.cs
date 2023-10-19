e782cd16ec3be5abb9008d38f483b58e45c44dbb
Alexandre Nonato <101572814+AlexandreNonato78@users.noreply.github.com>
2023-10-19 10:30:58 -0300

    Add files via upload

---
+++ 1810ExercicioFuncoes3/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _1810ExercicioFuncoes3
+    //3) Desenvolva uma função que calcule a média de três números fornecidos como argumentos.
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Digite três números para calcular a média:");
+
+            double numero1 = LerNumero("Digite o primeiro número: ");
+            double numero2 = LerNumero("Digite o segundo número: ");
+            double numero3 = LerNumero("Digite o terceiro número: ");
+
+            double media = CalcularMedia(numero1, numero2, numero3);
+
+            Console.WriteLine($"A média dos números é: {media}");
+        }
+
+        static double LerNumero(string mensagem)
+        {
+            Console.Write(mensagem);
+            double numero;
+            while (!double.TryParse(Console.ReadLine(), out numero))
+            {
+                Console.WriteLine("Entrada inválida. Por favor, insira um número válido.");
+                Console.Write(mensagem);
+            }
+            return numero;
+        }
+
+        static double CalcularMedia(double num1, double num2, double num3)
+        {
+            return (num1 + num2 + num3) / 3;
+        }
+    }
+}
