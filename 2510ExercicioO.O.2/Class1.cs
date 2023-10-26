04391c9560904883cf27312a2810006ffec0a92e
Alexandre Nonato <101572814+AlexandreNonato78@users.noreply.github.com>
2023-10-26 10:38:47 -0300

    Add files via upload

---
+++ 2510ExercicioO.O.2/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2510ExercicioO.O._2
+{
+    class Estudio
+    {
+        public string Nome { get; set; }
+        public int AnoFundacao { get; set; }
+        public string PaisOrigem { get; set; }
+        public int NumeroJogosProduzidos { get; set; }
+    }
+}
---
+++ 2510ExercicioO.O.2/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace _2510ExercicioO.O._2
+//2 - Crie uma classe "Estúdio" com os seguintes atributos:
+//Nome, Ano de fundação, País de origem e Número de jogos produzidos.
+//Na main, instancie um objeto e faça a leitura pelo teclado.
+{
+    using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe os dados do estúdio:");
+
+            Estudio meuEstudio = new Estudio(); // Instanciar um objeto da classe Estudio
+
+            Console.Write("Nome do estúdio: ");
+            meuEstudio.Nome = Console.ReadLine();
+
+            Console.Write("Ano de fundação: ");
+            if (int.TryParse(Console.ReadLine(), out int anoFundacao))
+            {
+                meuEstudio.AnoFundacao = anoFundacao;
+            }
+            else
+            {
+                Console.WriteLine("Ano de fundação inválido.");
+            }
+
+            Console.Write("País de origem: ");
+            meuEstudio.PaisOrigem = Console.ReadLine();
+
+            Console.Write("Número de jogos produzidos: ");
+            if (int.TryParse(Console.ReadLine(), out int numJogosProduzidos))
+            {
+                meuEstudio.NumeroJogosProduzidos = numJogosProduzidos;
+            }
+            else
+            {
+                Console.WriteLine("Número de jogos produzidos inválido.");
+            }
+
+            Console.WriteLine("\nDados do estúdio informados:");
+            Console.WriteLine("Nome do estúdio: " + meuEstudio.Nome);
+            Console.WriteLine("Ano de fundação: " + meuEstudio.AnoFundacao);
+            Console.WriteLine("País de origem: " + meuEstudio.PaisOrigem);
+            Console.WriteLine("Número de jogos produzidos: " + meuEstudio.NumeroJogosProduzidos);
+        }
+    }
+
+}
