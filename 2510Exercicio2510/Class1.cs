65a2d3bbff8890c6957ff150e9985b2d5ce89fec
Alexandre Nonato <101572814+AlexandreNonato78@users.noreply.github.com>
2023-10-26 10:37:19 -0300

    Add files via upload

---
+++ 2510Exercicio2510/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2510Exercicio2510
+{
+    class Jogo
+    {
+        public string Titulo { get; set; }
+        public string Genero { get; set; }
+        public string Plataforma { get; set; }
+        public int AnoLancamento { get; set; }
+    }
+}
---
+++ 2510Exercicio2510/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _2510Exercicio2510
+
+//1 - Crie em c# uma classe "Jogo" com os seguintes atributos:
+//Título, Gênero, Plataforma (por exemplo, PC, Xbox, PlayStation), Ano de lançamento.
+//Na main, instancie um objeto e faça a leitura pelo teclado.
+{
+    using System;
+
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe os dados do jogo:");
+
+            Jogo meuJogo = new Jogo(); // Instanciar um objeto da classe Jogo
+
+            Console.Write("Título: ");
+            meuJogo.Titulo = Console.ReadLine();
+
+            Console.Write("Gênero: ");
+            meuJogo.Genero = Console.ReadLine();
+
+            Console.Write("Plataforma: ");
+            meuJogo.Plataforma = Console.ReadLine();
+
+            Console.Write("Ano de lançamento: ");
+            if (int.TryParse(Console.ReadLine(), out int ano))
+            {
+                meuJogo.AnoLancamento = ano;
+            }
+            else
+            {
+                Console.WriteLine("Ano de lançamento inválido.");
+            }
+
+            Console.WriteLine("\nDados do jogo informados:");
+            Console.WriteLine("Título: " + meuJogo.Titulo);
+            Console.WriteLine("Gênero: " + meuJogo.Genero);
+            Console.WriteLine("Plataforma: " + meuJogo.Plataforma);
+            Console.WriteLine("Ano de lançamento: " + meuJogo.AnoLancamento);
+        }
+    }
+
+}
