73ff71b554b1ffd9b2fc659a9f57973c0d90b9fa
Alexandre Nonato <101572814+AlexandreNonato78@users.noreply.github.com>
2023-10-26 10:45:30 -0300

    Add files via upload

---
+++ 2510ExercicioO.O.4/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2510ExercicioO.O._4
+{
+    class Livro
+    {
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public int AnoPublicacao { get; set; }
+    }
+}
---
+++ 2510ExercicioO.O.4/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace _2510ExercicioO.O._4
+//4 - Crie uma classe Livro com os atributos título, autor e ano de publicação.
+//Faça a instanciação de 3 objetos e imprima as informações do livro na tela.
+{
+    using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Livro livro1 = new Livro
+            {
+                Titulo = "O Senhor dos Anéis: A Sociedade do Anel",
+                Autor = "J.R.R. Tolkien",
+                AnoPublicacao = 1954
+            };
+
+            Livro livro2 = new Livro
+            {
+                Titulo = "1984",
+                Autor = "George Orwell",
+                AnoPublicacao = 1949
+            };
+
+            Livro livro3 = new Livro
+            {
+                Titulo = "Dom Quixote",
+                Autor = "Miguel de Cervantes",
+                AnoPublicacao = 1605
+            };
+
+            Console.WriteLine("Informações dos Livros:");
+
+            Console.WriteLine("Livro 1:");
+            Console.WriteLine("Título: " + livro1.Titulo);
+            Console.WriteLine("Autor: " + livro1.Autor);
+            Console.WriteLine("Ano de Publicação: " + livro1.AnoPublicacao);
+
+            Console.WriteLine("\nLivro 2:");
+            Console.WriteLine("Título: " + livro2.Titulo);
+            Console.WriteLine("Autor: " + livro2.Autor);
+            Console.WriteLine("Ano de Publicação: " + livro2.AnoPublicacao);
+
+            Console.WriteLine("\nLivro 3:");
+            Console.WriteLine("Título: " + livro3.Titulo);
+            Console.WriteLine("Autor: " + livro3.Autor);
+            Console.WriteLine("Ano de Publicação: " + livro3.AnoPublicacao);
+        }
+    }
+
+}
