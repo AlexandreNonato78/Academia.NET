dd712d5cf5ae668b2ab94c8900b9b10f9cd0774e
Alexandre Nonato <101572814+AlexandreNonato78@users.noreply.github.com>
2023-10-30 11:12:42 -0300

    Add files via upload

---
+++ 2610ExercicioOrient.Obj.5/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _2610ExercicioOrient.Obj._5
+{
+    class Música
+    {
+        public string Nome { get; set; }
+        public string Autor { get; set; }
+        public string Gravadora { get; set; }
+
+        public Música(string nome, string autor, string gravadora)
+        {
+            Nome = nome;
+            Autor = autor;
+            Gravadora = gravadora;
+        }
+    }
+
+    class Playlist
+    {
+        public string Dono { get; set; }
+        private List<Música> músicas;
+        private Random random;
+
+        public Playlist(string dono)
+        {
+            Dono = dono;
+            músicas = new List<Música>();
+            random = new Random();
+        }
+
+        public void AdicionarMúsica(Música música)
+        {
+            músicas.Add(música);
+        }
+
+        public void TocarMúsicaAleatória()
+        {
+            if (músicas.Count > 0)
+            {
+                int índiceAleatório = random.Next(0, músicas.Count);
+                Música músicaTocada = músicas[índiceAleatório];
+                Console.WriteLine("Tocando a música: " + músicaTocada.Nome);
+            }
+            else
+            {
+                Console.WriteLine("Playlist vazia. Adicione músicas antes de tentar tocá-las.");
+            }
+        }
+    }
+}
---
+++ 2610ExercicioOrient.Obj.5/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+//5 - Crie uma classe chamada Música que contenha os atributos nome, autor e gravadora.
+//Após, crie uma classe Playlist que possua como atributo uma Lista de músicas e
+//uma string para armazenar o dono da playlist. Implemente um método para adicionar músicas na lista,
+//para "tocar a música" (só mostrar uma mensagem na tela com o titulo da música).
+//Tente fazer uma reprodução aleatória, ou seja, em vez de percorrer do inicio ao fim da lista,
+//faça um random para acessar valores aleatórios válidos.
+
+namespace _2610ExercicioOrient.Obj._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Playlist playlist = new Playlist("Seu Nome");
+
+            while (true)
+            {
+                Console.WriteLine("===== Menu da Playlist de Músicas =====");
+                Console.WriteLine("1 - Adicionar Música");
+                Console.WriteLine("2 - Tocar Música Aleatória");
+                Console.WriteLine("3 - Sair");
+                Console.Write("Escolha uma opção: ");
+
+                int escolha = int.Parse(Console.ReadLine());
+
+                switch (escolha)
+                {
+                    case 1:
+                        Console.Write("Nome da Música: ");
+                        string nomeMúsica = Console.ReadLine();
+                        Console.Write("Autor da Música: ");
+                        string autorMúsica = Console.ReadLine();
+                        Console.Write("Gravadora da Música: ");
+                        string gravadoraMúsica = Console.ReadLine();
+
+                        Música novaMúsica = new Música(nomeMúsica, autorMúsica, gravadoraMúsica);
+                        playlist.AdicionarMúsica(novaMúsica);
+                        Console.WriteLine("Música adicionada com sucesso.");
+                        break;
+
+                    case 2:
+                        playlist.TocarMúsicaAleatória();
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Saindo da Playlist de Músicas.");
+                        Environment.Exit(0);
+                        break;
+
+                    default:
+                        Console.WriteLine("Opção inválida. Tente novamente.");
+                        break;
+                }
+            }
+        }
+    }
+}
