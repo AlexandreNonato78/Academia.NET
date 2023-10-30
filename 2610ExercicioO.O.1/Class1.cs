c66343cc8617b36f05368a193dbaf08256bd74a1
Alexandre Nonato <101572814+AlexandreNonato78@users.noreply.github.com>
2023-10-30 11:06:13 -0300

    Add files via upload

---
+++ 2610ExercicioO.O.1/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _2610ExercicioO.O._1
+{
+    class Pessoa
+    {
+        // Atributos da classe Pessoa
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string RG { get; set; }
+    }
+}
---
+++ 2610ExercicioO.O.1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+//Exercicio 1 - Crie uma classe chamada Pessoa com seus atributos conforme sua preferência
+//(Digite: Nome, CPF, data de nascimento, RG etc). Crie métodos para cadastrar e apresentar na tela os dados desta classe.
+class Pessoa
+{
+    // Atributos da classe Pessoa
+    public string Nome { get; set; }
+    public string CPF { get; set; }
+    public DateTime DataNascimento { get; set; }
+    public string RG { get; set; }
+
+    // Método para cadastrar os dados da Pessoa
+    public void CadastrarPessoa(string nome, string cpf, DateTime dataNascimento, string rg)
+    {
+        Nome = nome;
+        CPF = cpf;
+        DataNascimento = dataNascimento;
+        RG = rg;
+    }
+
+    // Método para apresentar os dados da Pessoa na tela
+    public void ApresentarDados()
+    {
+        Console.WriteLine("Dados da Pessoa:");
+        Console.WriteLine("Nome: " + Nome);
+        Console.WriteLine("CPF: " + CPF);
+        Console.WriteLine("Data de Nascimento: " + DataNascimento.ToShortDateString());
+        Console.WriteLine("RG: " + RG);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Instanciar um objeto da classe Pessoa
+        Pessoa pessoa = new Pessoa();
+
+        // Solicitar ao usuário que insira os dados da Pessoa
+        Console.Write("Nome: ");
+        string nome = Console.ReadLine();
+
+        Console.Write("CPF: ");
+        string cpf = Console.ReadLine();
+
+        Console.Write("Data de Nascimento (yyyy-MM-dd): ");
+        if (DateTime.TryParse(Console.ReadLine(), out DateTime dataNascimento))
+        {
+            Console.Write("RG: ");
+            string rg = Console.ReadLine();
+
+            // Chamar o método para cadastrar os dados
+            pessoa.CadastrarPessoa(nome, cpf, dataNascimento, rg);
+
+            // Chamar o método para apresentar os dados
+            pessoa.ApresentarDados();
+        }
+        else
+        {
+            Console.WriteLine("Data de Nascimento inválida.");
+        }
+    }
+}
