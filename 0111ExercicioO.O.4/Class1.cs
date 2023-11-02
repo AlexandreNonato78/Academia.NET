c07f9d33a51075343988f7933b8cd3b7059fdfb7
Alexandre Nonato <101572814+AlexandreNonato78@users.noreply.github.com>
2023-11-02 15:07:11 -0300

    Add files via upload

---
+++ 0111ExercicioO.O.4/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace _0111ExercicioO.O._4
+{
+    class Carro
+    {
+        private string modelo;
+        public string Modelo
+        {
+            get { return modelo; }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                    modelo = value;
+                else
+                    Console.WriteLine("O modelo do carro não pode ser vazio.");
+            }
+        }
+
+        private int ano;
+        public int Ano
+        {
+            get { return ano; }
+            set
+            {
+                if (value >= 0)
+                    ano = value;
+                else
+                    Console.WriteLine("O ano do carro deve ser um número positivo.");
+            }
+        }
+
+        private int velocidade;
+        public int Velocidade
+        {
+            get { return velocidade; }
+            set
+            {
+                velocidade = Math.Max(value, 0); // Garante que a velocidade nunca seja negativa
+            }
+        }
+
+        public void Acelerar()
+        {
+            Velocidade += 10;
+        }
+
+        public void Frear()
+        {
+            Velocidade -= 10;
+        }
+    }
+}
---
+++ 0111ExercicioO.O.4/Program.cs
@@ -0,0 +1,76 @@
+﻿//11 - Crie uma classe "Carro" com os atributos "modelo", "ano" e "velocidade".
+//Em seguida, crie um método para acelerar o carro (aumentando a velocidade em 10) e
+//outro para frear o carro (diminuindo a velocidade em 10, mas nunca deixando a velocidade negativa).
+using System;
+
+namespace _0111ExercicioO.O._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro carro = new Carro();
+
+            Console.WriteLine("===== Cadastro de Carro =====");
+            do
+            {
+                Console.Write("Modelo do Carro: ");
+                carro.Modelo = Console.ReadLine();
+            } while (string.IsNullOrEmpty(carro.Modelo));
+
+            Console.Write("Ano do Carro: ");
+            if (int.TryParse(Console.ReadLine(), out int ano))
+            {
+                carro.Ano = ano;
+            }
+            else
+            {
+                Console.WriteLine("Ano inválido.");
+                return;
+            }
+
+            Console.Write("Velocidade Inicial do Carro: ");
+            if (int.TryParse(Console.ReadLine(), out int velocidade))
+            {
+                carro.Velocidade = velocidade;
+            }
+            else
+            {
+                Console.WriteLine("Velocidade inicial inválida.");
+                return;
+            }
+
+            Console.WriteLine("\n===== Operações com o Carro =====");
+            Console.WriteLine("1 - Acelerar");
+            Console.WriteLine("2 - Frear");
+            Console.WriteLine("3 - Sair");
+
+            while (true)
+            {
+                Console.Write("Escolha uma operação: ");
+                int escolha = int.Parse(Console.ReadLine());
+
+                switch (escolha)
+                {
+                    case 1:
+                        carro.Acelerar();
+                        Console.WriteLine("Carro acelerado. Nova velocidade: " + carro.Velocidade);
+                        break;
+
+                    case 2:
+                        carro.Frear();
+                        Console.WriteLine("Carro freado. Nova velocidade: " + carro.Velocidade);
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Saindo do programa.");
+                        return;
+
+                    default:
+                        Console.WriteLine("Opção inválida. Tente novamente.");
+                        break;
+                }
+            }
+        }
+    }
+}
