7be1045e63cd9fbbedbd40f6824a090ebd24b807
Alexandre Nonato <101572814+AlexandreNonato78@users.noreply.github.com>
2023-11-02 15:05:59 -0300

    Add files via upload

---
+++ 0111ExercicioO.O.2/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _0111ExercicioO.O._2
+{
+    class Autor
+    {
+        private string nome;
+        public string Nome
+        {
+            get { return nome; }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                    nome = value;
+                else
+                    Console.WriteLine("O nome do autor não pode ser vazio.");
+            }
+        }
+    }
+
+    class Livro
+    {
+        private string título;
+        public string Título
+        {
+            get { return título; }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                    título = value;
+                else
+                    Console.WriteLine("O título do livro não pode ser vazio.");
+            }
+        }
+
+        public Autor Autor { get; set; }
+    }
+}
---
+++ 0111ExercicioO.O.2/Program.cs
@@ -0,0 +1,35 @@
+﻿//9 - Crie uma classe chamada Livro que tenha as propriedades Titulo e Autor (outra classe).
+//Utilize o encapsulamento  para garantir que o título e o autor não sejam vazios.
+using System;
+
+namespace _0111ExercicioO.O._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Autor autor = new Autor();
+
+            Console.WriteLine("===== Cadastro de Autor =====");
+            do
+            {
+                Console.Write("Nome do Autor: ");
+                autor.Nome = Console.ReadLine();
+            } while (string.IsNullOrEmpty(autor.Nome));
+
+            Livro livro = new Livro();
+            livro.Autor = autor;
+
+            Console.WriteLine("\n===== Cadastro de Livro =====");
+            do
+            {
+                Console.Write("Título do Livro: ");
+                livro.Título = Console.ReadLine();
+            } while (string.IsNullOrEmpty(livro.Título));
+
+            Console.WriteLine("\n===== Informações do Livro =====");
+            Console.WriteLine("Título do Livro: " + livro.Título);
+            Console.WriteLine("Autor do Livro: " + livro.Autor.Nome);
+        }
+    }
+}
