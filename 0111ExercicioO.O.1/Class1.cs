521737f49e6187f221201679d129af29b9ac0514
Alexandre Nonato <101572814+AlexandreNonato78@users.noreply.github.com>
2023-11-02 15:05:17 -0300

    Add files via upload

---
+++ 0111ExercicioO.O.1/Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace ExercicioFabricanteProduto
+{
+    class Fabricante
+    {
+        private string nome;
+        public string Nome
+        {
+            get { return nome; }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                    nome = value;
+                else
+                    Console.WriteLine("O nome do fabricante não pode ser vazio.");
+            }
+        }
+
+        public string Endereço { get; set; }
+        public string Cidade { get; set; }
+    }
+
+    class Produto
+    {
+        private string nome;
+        public string Nome
+        {
+            get { return nome; }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                    nome = value;
+                else
+                {
+                    Console.WriteLine("O nome do produto não pode ser vazio.");
+                    return;
+                }
+            }
+        }
+
+        public Fabricante Fabricante { get; set; }
+
+        private double preco;
+        public double Preço
+        {
+            get { return preco; }
+            set
+            {
+                if (value >= 0)
+                    preco = value;
+                else
+                {
+                    Console.WriteLine("O preço do produto deve ser positivo.");
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ 0111ExercicioO.O.1/Program.cs
@@ -0,0 +1,57 @@
+﻿//8 - Crie classes chamadas Fabricante e Produto. Fabricante que tenha as propriedades Nome, Endereço e Cidade.
+//Produto que tenha as propriedades Nome, Fabricante (objeto da classe Fabricante) e Preco.
+//Utilize o encapsulamento para garantir que o nome não seja vazio e que o preço seja positivo. 
+
+using System;
+
+namespace ExercicioFabricanteProduto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fabricante fabricante = new Fabricante();
+
+            Console.WriteLine("===== Cadastro de Fabricante =====");
+            do
+            {
+                Console.Write("Nome do Fabricante: ");
+                fabricante.Nome = Console.ReadLine();
+            } while (string.IsNullOrEmpty(fabricante.Nome));
+
+            Console.Write("Endereço do Fabricante: ");
+            fabricante.Endereço = Console.ReadLine();
+            Console.Write("Cidade do Fabricante: ");
+            fabricante.Cidade = Console.ReadLine();
+
+            Produto produto = new Produto();
+            produto.Fabricante = fabricante;
+
+            Console.WriteLine("\n===== Cadastro de Produto =====");
+            do
+            {
+                Console.Write("Nome do Produto: ");
+                produto.Nome = Console.ReadLine();
+            } while (string.IsNullOrEmpty(produto.Nome));
+
+            Console.Write("Preço do Produto: ");
+            if (double.TryParse(Console.ReadLine(), out double preço))
+            {
+                produto.Preço = preço;
+            }
+            else
+            {
+                Console.WriteLine("Preço inválido.");
+                return;
+            }
+
+            Console.WriteLine("\n===== Informações do Produto =====");
+            Console.WriteLine("Nome do Produto: " + produto.Nome);
+            Console.WriteLine("Preço do Produto: R$ " + produto.Preço);
+            Console.WriteLine("Fabricante: " + produto.Fabricante.Nome);
+            Console.WriteLine("Endereço do Fabricante: " + produto.Fabricante.Endereço);
+            Console.WriteLine("Cidade do Fabricante: " + produto.Fabricante.Cidade);
+        }
+    }
+}
+
