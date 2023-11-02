c4df414f74db553f2e15b09c4b1285aa571a0ce9
Alexandre Nonato <101572814+AlexandreNonato78@users.noreply.github.com>
2023-11-02 15:06:37 -0300

    Add files via upload

---
+++ 0111ExercicioO.O.3/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _0111ExercicioO.O._3
+{
+    class Animal
+    {
+        private string nome;
+        public string Nome
+        {
+            get { return nome; }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                    nome = value;
+                else
+                    Console.WriteLine("O nome do animal não pode ser vazio.");
+            }
+        }
+
+        private string espécie;
+        public string Espécie
+        {
+            get { return espécie; }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                    espécie = value;
+                else
+                    Console.WriteLine("A espécie do animal não pode ser vazia.");
+            }
+        }
+
+        private int idade;
+        public int Idade
+        {
+            get { return idade; }
+            set
+            {
+                if (value >= 0)
+                    idade = value;
+                else
+                    Console.WriteLine("A idade do animal deve ser um número positivo.");
+            }
+        }
+
+        public void EmitirSom(string som)
+        {
+            Console.WriteLine("O som do animal é: " + som);
+        }
+    }
+}
---
+++ 0111ExercicioO.O.3/Program.cs
@@ -0,0 +1,48 @@
+﻿//10 - Crie uma classe chamada Animal que tenha as propriedades Nome, Especie e Idade.
+//Crie também um método chamado EmitirSom, que imprime na tela o som do animal.
+
+using System;
+
+namespace _0111ExercicioO.O._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal();
+
+            Console.WriteLine("===== Cadastro de Animal =====");
+            do
+            {
+                Console.Write("Nome do Animal: ");
+                animal.Nome = Console.ReadLine();
+            } while (string.IsNullOrEmpty(animal.Nome));
+
+            do
+            {
+                Console.Write("Espécie do Animal: ");
+                animal.Espécie = Console.ReadLine();
+            } while (string.IsNullOrEmpty(animal.Espécie));
+
+            Console.Write("Idade do Animal: ");
+            if (int.TryParse(Console.ReadLine(), out int idade))
+            {
+                animal.Idade = idade;
+            }
+            else
+            {
+                Console.WriteLine("Idade inválida.");
+                return;
+            }
+
+            Console.WriteLine("Som emitido pelo Animal: ");
+            string som = Console.ReadLine();
+            animal.EmitirSom(som);
+
+            Console.WriteLine("\n===== Informações do Animal =====");
+            Console.WriteLine("Nome do Animal: " + animal.Nome);
+            Console.WriteLine("Espécie do Animal: " + animal.Espécie);
+            Console.WriteLine("Idade do Animal: " + animal.Idade);
+        }
+    }
+}
