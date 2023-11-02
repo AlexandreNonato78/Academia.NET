c9919ad2d07e406270547c73342eb405e9e41839
Alexandre Nonato <101572814+AlexandreNonato78@users.noreply.github.com>
2023-11-02 15:08:07 -0300

    Add files via upload

---
+++ 0111ExercicioO.O.5/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _0111ExercicioO.O._5
+{
+    class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public float Altura { get; set; }
+    }
+
+    class Agenda
+    {
+        private List<Pessoa> pessoas;
+
+        public Agenda()
+        {
+            pessoas = new List<Pessoa>();
+        }
+
+        public void ArmazenaPessoa(string nome, int idade, float altura)
+        {
+            Pessoa pessoa = new Pessoa
+            {
+                Nome = nome,
+                Idade = idade,
+                Altura = altura
+            };
+            pessoas.Add(pessoa);
+        }
+
+        public void RemovePessoa(string nome)
+        {
+            pessoas.RemoveAll(p => p.Nome == nome);
+        }
+
+        public Pessoa BuscaPessoa(string nome)
+        {
+            return pessoas.Find(p => p.Nome == nome);
+        }
+
+        public void ImprimeAgenda()
+        {
+            Console.WriteLine("\n===== Agenda de Pessoas =====");
+            foreach (var pessoa in pessoas)
+            {
+                Console.WriteLine($"Nome: {pessoa.Nome}, Idade: {pessoa.Idade}, Altura: {pessoa.Altura}");
+            }
+        }
+    }
+}
+
---
+++ 0111ExercicioO.O.5/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace _0111ExercicioO.O._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Agenda agenda = new Agenda();
+
+            while (true)
+            {
+                Console.WriteLine("\n===== Menu da Agenda =====");
+                Console.WriteLine("1 - Adicionar Pessoa");
+                Console.WriteLine("2 - Remover Pessoa");
+                Console.WriteLine("3 - Buscar Pessoa");
+                Console.WriteLine("4 - Imprimir Agenda");
+                Console.WriteLine("5 - Sair");
+                Console.Write("Escolha uma opção: ");
+
+                int escolha = int.Parse(Console.ReadLine());
+
+                switch (escolha)
+                {
+                    case 1:
+                        Console.Write("Nome da Pessoa: ");
+                        string nome = Console.ReadLine();
+                        Console.Write("Idade da Pessoa: ");
+                        int idade = int.Parse(Console.ReadLine());
+                        Console.Write("Altura da Pessoa: ");
+                        float altura = float.Parse(Console.ReadLine());
+
+                        agenda.ArmazenaPessoa(nome, idade, altura);
+                        Console.WriteLine("Pessoa adicionada à agenda.");
+                        break;
+
+                    case 2:
+                        Console.Write("Nome da Pessoa a ser removida: ");
+                        string nomeRemover = Console.ReadLine();
+                        agenda.RemovePessoa(nomeRemover);
+                        Console.WriteLine("Pessoa removida da agenda.");
+                        break;
+
+                    case 3:
+                        Console.Write("Nome da Pessoa a ser buscada: ");
+                        string nomeBuscar = Console.ReadLine();
+                        Pessoa pessoaEncontrada = agenda.BuscaPessoa(nomeBuscar);
+                        if (pessoaEncontrada != null)
+                            Console.WriteLine($"Pessoa encontrada - Nome: {pessoaEncontrada.Nome}, Idade: {pessoaEncontrada.Idade}, Altura: {pessoaEncontrada.Altura}");
+                        else
+                            Console.WriteLine("Pessoa não encontrada na agenda.");
+                        break;
+
+                    case 4:
+                        agenda.ImprimeAgenda();
+                        break;
+
+                    case 5:
+                        Console.WriteLine("Saindo do programa.");
+                        Environment.Exit(0);
+                        break;
+
+                    default:
+                        Console.WriteLine("Opção inválida. Tente novamente.");
+                        break;
+                }
+            }
+        }
+    }
+}
