0ac97400517dd17d539c33287d1f6c1bb9c8d8ce
Alexandre Nonato <101572814+AlexandreNonato78@users.noreply.github.com>
2023-11-08 10:56:21 -0300

    Add files via upload

---
+++ 061123_O.O_Abstração6/Class1.cs
@@ -0,0 +1,44 @@
+﻿namespace _0611_O.O_Abstração6;
+using System;
+
+
+public class ContaBancaria
+{
+    public double Saldo { get; private set; }
+
+    public ContaBancaria(double saldoInicial)
+    {
+        Saldo = saldoInicial;
+    }
+
+    public void Depositar(double valor)
+    {
+        if (valor > 0)
+        {
+            Saldo += valor;
+            Console.WriteLine($"Depósito de {valor:C} efetuado com sucesso.");
+        }
+        else
+        {
+            Console.WriteLine("Valor de depósito inválido.");
+        }
+    }
+
+    public void Sacar(double valor)
+    {
+        if (valor > 0 && valor <= Saldo)
+        {
+            Saldo -= valor;
+            Console.WriteLine($"Saque de {valor:C} efetuado com sucesso.");
+        }
+        else
+        {
+            Console.WriteLine("Saldo insuficiente ou valor de saque inválido.");
+        }
+    }
+
+    public void MostrarSaldo()
+    {
+        Console.WriteLine($"Saldo atual: {Saldo:C}");
+    }
+}
---
+++ 061123_O.O_Abstração6/Program.cs
@@ -0,0 +1,50 @@
+﻿//6 - Crie uma classe "ContaBancaria" com propriedades para "Saldo" e métodos "Depositar" e "Sacar".
+
+namespace _0611_O.O_Abstração6;
+using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite o saldo inicial da conta bancária: ");
+        double saldoInicial = double.Parse(Console.ReadLine());
+
+        ContaBancaria conta = new ContaBancaria(saldoInicial);
+
+        while (true)
+        {
+            Console.WriteLine("\nEscolha uma opção:");
+            Console.WriteLine("1 - Depositar");
+            Console.WriteLine("2 - Sacar");
+            Console.WriteLine("3 - Mostrar Saldo");
+            Console.WriteLine("4 - Sair");
+
+            int escolha = int.Parse(Console.ReadLine());
+
+            switch (escolha)
+            {
+                case 1:
+                    Console.Write("Digite o valor para depositar: ");
+                    double valorDeposito = double.Parse(Console.ReadLine());
+                    conta.Depositar(valorDeposito);
+                    break;
+                case 2:
+                    Console.Write("Digite o valor para sacar: ");
+                    double valorSaque = double.Parse(Console.ReadLine());
+                    conta.Sacar(valorSaque);
+                    break;
+                case 3:
+                    conta.MostrarSaldo();
+                    break;
+                case 4:
+                    Console.WriteLine("Encerrando o programa.");
+                    return;
+                default:
+                    Console.WriteLine("Opção inválida. Tente novamente.");
+                    break;
+            }
+        }
+    }
+}
