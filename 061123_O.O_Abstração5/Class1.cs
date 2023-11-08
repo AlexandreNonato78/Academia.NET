3ca36b9c3087c4a0744645a1928cf94f564aceb4
Alexandre Nonato <101572814+AlexandreNonato78@users.noreply.github.com>
2023-11-08 10:55:50 -0300

    Add files via upload

---
+++ 061123_O.O_Abstração5/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _0611_O.O_Abstração5;
+
+public abstract class Produto
+{
+    public string Nome { get; protected set; }
+    public decimal Preco { get; protected set; }
+
+    public Produto(string nome, decimal preco)
+    {
+        Nome = nome;
+        Preco = preco;
+    }
+
+    public abstract decimal CalcularDesconto();
+}
+
+public class Livro : Produto
+{
+    public Livro(string nome, decimal preco) : base(nome, preco)
+    {
+    }
+
+    public override decimal CalcularDesconto()
+    {
+        return Preco * 0.05m;
+    }
+}
+
+public class Eletronico : Produto
+{
+    public Eletronico(string nome, decimal preco) : base(nome, preco)
+    {
+    }
+
+    public override decimal CalcularDesconto()
+    {
+        return Preco * 0.125m;
+    }
+}
+
---
+++ 061123_O.O_Abstração5/Program.cs
@@ -0,0 +1,49 @@
+﻿//5 - Crie uma classe abstrata "Produto" com propriedades para "Nome", "Preço" e um método abstrato "CalcularDesconto".
+//Crie classes derivadas para diferentes tipos de produtos, como "Livro" e "Eletrônico", que implementam o método "CalcularDesconto"
+//de acordo com as regras específicas de desconto para cada tipo de produto.
+//Livro – 5% de desconto
+//Eletrônico  - 12.5% de desconto # os dados deve ser inseridos pelo usuário
+
+namespace _0611_O.O_Abstração5;
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite o nome do produto: ");
+        string nomeProduto = Console.ReadLine();
+
+        Console.Write("Digite o preço do produto: ");
+        decimal precoProduto = decimal.Parse(Console.ReadLine());
+
+        Console.WriteLine("Selecione o tipo de produto:");
+        Console.WriteLine("1 - Livro");
+        Console.WriteLine("2 - Eletrônico");
+        int escolha = int.Parse(Console.ReadLine());
+
+        Produto produto;
+
+        if (escolha == 1)
+        {
+            produto = new Livro(nomeProduto, precoProduto);
+        }
+        else if (escolha == 2)
+        {
+            produto = new Eletronico(nomeProduto, precoProduto);
+        }
+        else
+        {
+            Console.WriteLine("Opção inválida.");
+            return;
+        }
+
+        decimal desconto = produto.CalcularDesconto();
+
+        Console.WriteLine($"Nome do produto: {produto.Nome}");
+        Console.WriteLine($"Preço do produto: {produto.Preco:C}");
+        Console.WriteLine($"Desconto aplicado: {desconto:C}");
+        Console.WriteLine($"Preço com desconto: {produto.Preco - desconto:C}");
+    }
+}
