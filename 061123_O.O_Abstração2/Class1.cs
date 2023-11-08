fa46a23e6e413debae7e34576a0148fe8cd63600
Alexandre Nonato <101572814+AlexandreNonato78@users.noreply.github.com>
2023-11-08 10:53:22 -0300

    Add files via upload

---
+++ 061123_O.O_Abstração2/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+public abstract class Veiculo
+{
+    public string Marca { get; set; }
+    public string Modelo { get; set; }
+
+    public Veiculo(string marca, string modelo)
+    {
+        Marca = marca;
+        Modelo = modelo;
+    }
+
+    public abstract void Dirigir();
+}
+
+public class Carro : Veiculo
+{
+    public int QuantidadeDePortas { get; set; }
+
+    public Carro(string marca, string modelo, int quantidadeDePortas) : base(marca, modelo)
+    {
+        QuantidadeDePortas = quantidadeDePortas;
+    }
+
+    public override void Dirigir()
+    {
+        Console.WriteLine($"Dirigindo o {Marca} {Modelo} com {QuantidadeDePortas} portas.");
+    }
+}
+
+public class Moto : Veiculo
+{
+    public int Cilindradas { get; set; }
+
+    public Moto(string marca, string modelo, int cilindradas) : base(marca, modelo)
+    {
+        Cilindradas = cilindradas;
+    }
+
+    public override void Dirigir()
+    {
+        Console.WriteLine($"Dirigindo a {Marca} {Modelo} com {Cilindradas} cilindradas.");
+    }
+}
---
+++ 061123_O.O_Abstração2/Program.cs
@@ -0,0 +1,39 @@
+﻿//2 - Criar uma classe Veiculo que contém as propriedades Marca e Modelo.
+//Criar um método abstrato chamado Dirigir. Criar duas classes derivadas, Carro e Moto, que herdam de Veiculo.
+//A classe Carro deve ter uma propriedade adicional QuantidadeDePortas, enquanto a classe Moto deve ter uma propriedade adicional Cilindrada.
+//Criar um método abstrato Dirigir na classe Veiculo. Em seguida, criar um método Dirigir na classe Carro
+//que imprime "Dirigindo o <marca> <modelo> com <quantidadeDePortas> portas" e
+//um método Dirigir na classe Moto que imprime "Dirigindo a <marca> <modelo> com <cilindrada> cilindradas"
+
+namespace _0611_O.O_Abstração2;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Informações do Carro:");
+        Console.Write("Marca: ");
+        string marcaCarro = Console.ReadLine();
+        Console.Write("Modelo: ");
+        string modeloCarro = Console.ReadLine();
+        Console.Write("Quantidade de Portas: ");
+        int quantidadeDePortas = int.Parse(Console.ReadLine());
+
+        Carro carro = new Carro(marcaCarro, modeloCarro, quantidadeDePortas);
+
+        Console.WriteLine("\nInformações da Moto:");
+        Console.Write("Marca: ");
+        string marcaMoto = Console.ReadLine();
+        Console.Write("Modelo: ");
+        string modeloMoto = Console.ReadLine();
+        Console.Write("Cilindradas: ");
+        int cilindradas = int.Parse(Console.ReadLine());
+
+        Moto moto = new Moto(marcaMoto, modeloMoto, cilindradas);
+
+        Console.WriteLine("\nIniciar a condução:");
+        carro.Dirigir();
+        moto.Dirigir();
+    }
+}
