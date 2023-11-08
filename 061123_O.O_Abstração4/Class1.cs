d0e7799f428e9bf8bf489b5a45d66bc144617906
Alexandre Nonato <101572814+AlexandreNonato78@users.noreply.github.com>
2023-11-08 10:55:09 -0300

    Add files via upload

---
+++ 061123_O.O_Abstração4/Class1.cs
@@ -0,0 +1,64 @@
+﻿namespace _0611_O.O_Abstração4;
+
+using System;
+
+public abstract class ContaBancaria
+{
+    public double Saldo { get; protected set; }
+
+    public ContaBancaria(double saldoInicial)
+    {
+        Saldo = saldoInicial;
+    }
+
+    public abstract void Depositar(double valor);
+    public abstract void Sacar(double valor);
+}
+
+public class ContaCorrente : ContaBancaria
+{
+    public ContaCorrente(double saldoInicial) : base(saldoInicial)
+    {
+    }
+
+    public override void Depositar(double valor)
+    {
+        Saldo += valor;
+    }
+
+    public override void Sacar(double valor)
+    {
+        if (valor > 0 && valor <= Saldo)
+        {
+            Saldo -= valor;
+        }
+        else
+        {
+            Console.WriteLine("Saldo insuficiente ou valor de saque inválido.");
+        }
+    }
+}
+
+public class ContaPoupanca : ContaBancaria
+{
+    public ContaPoupanca(double saldoInicial) : base(saldoInicial)
+    {
+    }
+
+    public override void Depositar(double valor)
+    {
+        Saldo += valor;
+    }
+
+    public override void Sacar(double valor)
+    {
+        if (valor > 0 && valor <= Saldo)
+        {
+            Saldo -= valor;
+        }
+        else
+        {
+            Console.WriteLine("Saldo insuficiente ou valor de saque inválido.");
+        }
+    }
+}
---
+++ 061123_O.O_Abstração4/Program.cs
@@ -0,0 +1,40 @@
+﻿//4 - Crie uma classe abstrata "ContaBancaria" com propriedades para "Saldo" e métodos abstratos "Depositar" e "Sacar".
+//Crie classes derivadas, como "ContaCorrente" e "ContaPoupanca", que implementam os métodos de depósito e saque de acordo com as regras de cada tipo de conta.
+
+namespace _0611_O.O_Abstração4;
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite o saldo inicial da Conta Corrente: ");
+        double saldoInicialContaCorrente = double.Parse(Console.ReadLine());
+        ContaCorrente contaCorrente = new ContaCorrente(saldoInicialContaCorrente);
+
+        Console.Write("Digite o saldo inicial da Conta Poupança: ");
+        double saldoInicialContaPoupanca = double.Parse(Console.ReadLine());
+        ContaPoupanca contaPoupanca = new ContaPoupanca(saldoInicialContaPoupanca);
+
+        Console.WriteLine("\nSaldo inicial da Conta Corrente: " + contaCorrente.Saldo);
+        Console.WriteLine("Saldo inicial da Conta Poupança: " + contaPoupanca.Saldo);
+
+        Console.Write("\nDigite o valor a depositar na Conta Corrente: ");
+        double depositoContaCorrente = double.Parse(Console.ReadLine());
+        contaCorrente.Depositar(depositoContaCorrente);
+
+        Console.Write("Digite o valor a sacar da Conta Corrente: ");
+        double saqueContaCorrente = double.Parse(Console.ReadLine());
+        contaCorrente.Sacar(saqueContaCorrente);
+
+        Console.Write("\nDigite o valor a depositar na Conta Poupança: ");
+        double depositoContaPoupanca = double.Parse(Console.ReadLine());
+        contaPoupanca.Depositar(depositoContaPoupanca);
+
+        Console.Write("Digite o valor a sacar da Conta Poupança: ");
+        double saqueContaPoupanca = double.Parse(Console.ReadLine());
+        contaPoupanca.Sacar(saqueContaPoupanca);
+
+        Console.WriteLine("\nSaldo atual da Conta Corrente: " + contaCorrente.Saldo);
+        Console.WriteLine("Saldo atual da Conta Poupança: " + contaPoupanca.Saldo);
+    }
+}
