b9e900edcd07144854a681bb30127c2685eaaed2
Alexandre Nonato <101572814+AlexandreNonato78@users.noreply.github.com>
2023-11-08 10:54:17 -0300

    Add files via upload

---
+++ 061123_O.O_Abstração3/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _0611_O.O_Abstração3;
+
+public interface IFormaGeometrica
+{
+    double CalcularArea();
+    double CalcularPerimetro();
+}
+
+public class Retangulo : IFormaGeometrica
+{
+    public double Largura { get; set; }
+    public double Altura { get; set; }
+
+    public Retangulo(double largura, double altura)
+    {
+        Largura = largura;
+        Altura = altura;
+    }
+
+    public double CalcularArea()
+    {
+        return Largura * Altura;
+    }
+
+    public double CalcularPerimetro()
+    {
+        return 2 * (Largura + Altura);
+    }
+}
+
+public class Circulo : IFormaGeometrica
+{
+    public double Raio { get; set; }
+
+    public Circulo(double raio)
+    {
+        Raio = raio;
+    }
+
+    public double CalcularArea()
+    {
+        return Math.PI * Math.Pow(Raio, 2);
+    }
+
+    public double CalcularPerimetro()
+    {
+        return 2 * Math.PI * Raio;
+    }
+}
---
+++ 061123_O.O_Abstração3/Program.cs
@@ -0,0 +1,34 @@
+﻿//3 - Crie uma interface chamada "IFormaGeometrica" com dois métodos abstratos: "CalcularArea()" e "CalcularPerimetro()".
+//Em seguida, crie duas classes que implementam essa interface: "Retangulo" e "Circulo".
+//Implemente os métodos "CalcularArea()" e "CalcularPerimetro()" para cada uma dessas classes,
+//de forma que o "Retangulo" calcule a área e o perímetro de um retângulo e o "Circulo" calcule a área e o perímetro de um círculo.
+using System;
+namespace _0611_O.O_Abstração3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite as informações do Retângulo:");
+        Console.Write("Largura: ");
+        double largura = double.Parse(Console.ReadLine());
+        Console.Write("Altura: ");
+        double altura = double.Parse(Console.ReadLine());
+
+        Retangulo retangulo = new Retangulo(largura, altura);
+
+        Console.WriteLine("\nDigite as informações do Círculo:");
+        Console.Write("Raio: ");
+        double raio = double.Parse(Console.ReadLine());
+
+        Circulo circulo = new Circulo(raio);
+
+        Console.WriteLine("\nÁrea e Perímetro do Retângulo:");
+        Console.WriteLine($"Área: {retangulo.CalcularArea()}");
+        Console.WriteLine($"Perímetro: {retangulo.CalcularPerimetro()}");
+
+        Console.WriteLine("\nÁrea e Perímetro do Círculo:");
+        Console.WriteLine($"Área: {circulo.CalcularArea()}");
+        Console.WriteLine($"Perímetro: {circulo.CalcularPerimetro()}");
+    }
+}
