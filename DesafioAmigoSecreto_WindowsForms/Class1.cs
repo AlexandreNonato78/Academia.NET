39185a319ab875cd6f9bdc8d9c70f92aac5b66a4
Alexandre Nonato <101572814+AlexandreNonato78@users.noreply.github.com>
2023-11-17 12:48:40 -0300

    Add files via upload

---
+++ DesafioAmigoSecreto_WindowsForms/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioWindowsForms2
+{
+    internal class Amigo
+    {
+        string nome;
+        string email;
+
+        public Amigo(string nome, string email)
+        {
+            this.Nome = nome;
+            this.Email = email;
+        }
+        public string Nome { get => nome; set => nome = value; }
+        public string Email { get => email; set => email = value; }
+
+        public static bool amigoCadastrado(string nome, List<Amigo>lista)
+        {
+            foreach (Amigo i in lista) 
+            {
+                if (nome.Equals(i.Nome)) 
+                {
+                    return true;
+                }                  
+            }
+            return false; //amigo não localizado na lista
+        }
+    }
+
+}
